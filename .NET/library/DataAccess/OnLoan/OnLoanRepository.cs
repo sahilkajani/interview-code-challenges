e361c17af64dab7be7de6f8bdd230435a1f456d3
Sahil <sahilkajani30@gmail.com>
2025-03-20 20:13:10 +0000

    Refactoring

--- .NET/library/DataAccess/OnLoan/OnLoanRepository.cs
+++ .NET/library/DataAccess/OnLoan/OnLoanRepository.cs
@@ -4,23 +4,26 @@ namespace OneBeyondApi.DataAccess.OnLoan
 {
     public class OnLoanRepository : IOnLoanRepository
     {
+        private readonly LibraryContext _context;
+
+        public OnLoanRepository(LibraryContext context)
+        {
+            _context = context;
+        }
+
         public IEnumerable<OneBeyondApi.Model.OnLoan> GetBooksOnLoan()
         {
-            using (var context = new LibraryContext())
-            {
-                  return context.Catalogue
-                           .Include(x => x.Book)
-                           .Include(x => x.OnLoanTo)
-                           .Where(x => x.OnLoanTo != null)
-                           .Select(x => new OneBeyondApi.Model.OnLoan()
-                           {
-                               BookName = x.Book.Name,
-                               BorrowerEmailAddress = x.OnLoanTo.EmailAddress,
-                               BorrowerName = x.OnLoanTo.Name
+            return _context.Catalogue
+                    .Include(x => x.Book)
+                    .Include(x => x.OnLoanTo)
+                    .Where(x => x.OnLoanTo != null)
+                    .Select(x => new OneBeyondApi.Model.OnLoan()
+                    {
+                        BookName = x.Book.Name,
+                        BorrowerEmailAddress = x.OnLoanTo.EmailAddress,
+                        BorrowerName = x.OnLoanTo.Name
 
-                           }).ToList();
-            }
-
+                    }).ToList();
         }
     }
 }
